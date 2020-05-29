a0a91bc46732d8e0f418bd4c52c981c92d24405d
GABRIEL ALEXANDER SANTANA CONTRERAS <1085855@est.intec.edu.do>
2020-05-28 20:07:06 -0400

    Add project files.

---
+++ Asignacion_3_INS368L_02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Asignacion_3_INS368L_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la longitud");
+            String result = Console.ReadLine();
+            
+            string [] intList = result.Select(q => new string(q, 1)).ToArray();
+            int[] myInts = Array.ConvertAll(intList, int.Parse);
+            
+            int count = 0;
+            while (myInts.Length > 1)
+            {
+                int sum = 0;
+                for (int i = 0; i < myInts.Length; i++)
+                {
+                    sum += myInts[i];
+                    
+                }
+                
+                string[] arr = Convert.ToString(sum).Select(q => new string(q, 1)).ToArray();
+                myInts = Array.ConvertAll(arr, int.Parse);
+                count++;
+            }
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+
+       
+    }
+}
